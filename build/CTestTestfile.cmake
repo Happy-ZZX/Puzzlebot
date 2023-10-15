8216c41c00a80a803019c78e7c72ce823503099f
ZZX <709463163@qq.com>
2023-10-15 17:28:34 +0100

    initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zzx/Puzzle_Bot/src
+# Build directory: /home/zzx/Puzzle_Bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
